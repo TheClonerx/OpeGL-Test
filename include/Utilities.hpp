ee0152e1890ffeee19d5b0c78d78352ddaf9976e
TheClonerx <theclonerx@outlook.com>
2020-04-18 17:36:02 -0400

    Added tcx::floor_division & tcx::positive_modulus

--- include/Utilities.hpp
+++ include/Utilities.hpp
@@ -23,6 +23,65 @@ struct VectorHasher<glm::vec<I, T, Q>> {
         return std::hash<std::string_view> {}({ reinterpret_cast<char const*>(&v), sizeof(v) });
     }
 };
+
+template <typename R, typename T, typename F, std::size_t... I, typename... Args>
+R for_each_index(T&& o, F&& f, std::index_sequence<I...>, Args&&... args)
+{
+    return R { f(o[I], std::forward<Args>(args)...)... };
+}
+
+template <typename T0, typename T1>
+auto positive_modulus(T0 x, T1 y) noexcept //similar to x % y in python
+{
+    static_assert((std::is_integral_v<T0> && std::is_integral_v<T1>) || (std::is_floating_point_v<T0> && std::is_floating_point_v<T1>), "Either both types are integer, or both types are floating point");
+
+    if constexpr (std::is_integral_v<T0>) {
+        return static_cast<std::make_unsigned_t<T0>>(x) % y;
+    } else {
+        auto result = std::fmod(x, y);
+        if (result && x < 0)
+            return -result;
+        else
+            return result;
+    }
+}
+
+template <glm::length_t I, typename T0, typename T1, glm::qualifier Q>
+auto positive_modulus(glm::vec<I, T0, Q> const& x, T1 y) noexcept //calls positive_modulus(x[i], y) for each value on the vector
+{
+    static_assert((std::is_integral_v<T0> && std::is_integral_v<T1>) || (std::is_floating_point_v<T0> && std::is_floating_point_v<T1>), "Either both types are integer, or both types are floating point");
+
+    auto f = positive_modulus<T0, T1>;
+    if constexpr (std::is_integral_v<T0>) {
+        return for_each_index<glm::vec<I, std::make_unsigned_t<T0>, Q>>(x, f, std::make_index_sequence<I> {}, y);
+    } else {
+        return for_each_index<glm::vec<I, T0, Q>>(x, f, std::make_index_sequence<I> {}, y);
+    }
+}
+
+template <typename T0, typename T1>
+auto floor_division(T0 x, T1 y) noexcept //similar to x//y in python
+{
+    using result_type = decltype(x / y);
+    if constexpr (std::is_integral_v<result_type>) {
+        if ((x < 0 && y < 0) || (x > 0 && y > 0)) { // same sign, do nothing
+            return x / y;
+        } else {
+            return x / y - 1;
+        }
+    } else {
+        return std::floor(x / y);
+    }
+}
+
+template <glm::length_t I, typename T0, typename T1, glm::qualifier Q>
+auto floor_division(glm::vec<I, T0, Q> const& x, T1 y) noexcept //calls floor_division(x[i], y) for each value on the vector
+{
+    using result_type = decltype(std::declval<T0>() / y);
+    auto f = floor_division<T0, T1>;
+    return for_each_index<glm::vec<I, result_type, Q>>(x, f, std::make_index_sequence<I> {}, y);
+}
+
 class TimeIt {
 public:
     TimeIt(std::string_view name)
